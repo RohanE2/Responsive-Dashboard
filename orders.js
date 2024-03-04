6ab489c0b1af5a8d9e571f7acfe871732035fccc
Rohan <rohanitu@gmail.com>
2024-03-04 18:56:22 +0530

    first commit

---
+++ orders.js
@@ -0,0 +1,51 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber:'85631',
+        productStatus:'Due',
+        shipping:'Pending'
+
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber:'36378',
+        productStatus:'Refunded',
+        shipping:'Declined'
+
+    },
+    {
+        productName: 'Rulo F11 Pro Drone',
+        productNumber:'49347',
+        productStatus:'Due',
+        shipping:'Pending'
+
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber:'96996',
+        productStatus:'paid',
+        shipping:'Delivered'
+
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber:'22021',
+        productStatus:'Paid',
+        shipping:'Delivered'
+
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber:'81475',
+        productStatus:'Due',
+        shipping:'Pending'
+
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber:'00482',
+        productStatus:'Paid',
+        shipping:'Delivered'
+
+    }
+]
